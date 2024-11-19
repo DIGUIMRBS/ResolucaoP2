a8745ff296ba2d96f166f785f777ece292dee5a7
Rodrigo <rodrigo@gmail.com>
2024-11-19 20:02:40 -0300

    Resolicao Questão 2

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
+from .models import Todo
 
-# Create your views here.
+class TodoListView(ListView):
+ model = Todo
